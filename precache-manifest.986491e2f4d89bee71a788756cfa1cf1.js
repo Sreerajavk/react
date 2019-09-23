154f4ed8c99dde69dcb2ee18f4ca175056990343
sreeraj <sreerajavk123@gmail.com>
2019-09-23 14:36:53 +0530

    Updates

---
+++ precache-manifest.986491e2f4d89bee71a788756cfa1cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2a7786032a0b30231f1261b77bd2b2",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "23d76b76e6c366c8fd10",
+    "url": "/react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d6bf1b54c888c695e9c2",
+    "url": "/react/static/js/2.e910a405.chunk.js"
+  },
+  {
+    "revision": "23d76b76e6c366c8fd10",
+    "url": "/react/static/js/main.5cc3998f.chunk.js"
+  },
+  {
+    "revision": "5791201faf69ecaa5ed2",
+    "url": "/react/static/js/runtime-main.611b14f1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react/static/media/logo.25bf045c.svg"
+  }
+]);
