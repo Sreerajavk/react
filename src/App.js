42b27ecce5a9b27f8ee05e412ee1ffc19e764f2b
sreeraj <sreerajavk123@gmail.com>
2019-10-12 13:42:23 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
+import Change from './Person/change'
 // import Radium from "radium";
-import  {SlideDown} from "react-slidedown";
+import {SlideDown} from "react-slidedown";
 import 'react-slidedown/lib/slidedown.css';
+import axios from 'axios';
 
 // class App extends React.Component{
 //   constructor(){
@@ -42,34 +44,39 @@ class App extends Component {
 
     state = {
         persons: [
-            {id : "dfsdf","Name": "Sreeraj", "Age": 22 },
-            {id: "sdfsdf","Name": "Sree", "Age": 22 },
+            {id: "dfsdf", "Name": "Sreeraj", "Age": 22},
+            {id: "sdfsdf", "Name": "Sree", "Age": 22},
         ],
-        show : false
+        show: false,
+        post : []
     };
+    style_ = {
+        width: '100px',
+        height: '100px'
+    }
     // constructor(){
     //     super();
     //     this.changeText = this.changeText.bind(this);
     //     this.change = this.change.bind(this);
     // }
 
-    change = ()=>{
+    change = () => {
         console.log('clicked');
 
         this.setState({
-            show : !this.state.show
+            show: !this.state.show
         })
     }
-    DeleteHandler = (index)=>{
+    DeleteHandler = (index) => {
         const persons = [...this.state.persons];
-        persons.splice(index , 1);
-        this.setState({persons : persons})
+        persons.splice(index, 1);
+        this.setState({persons: persons})
     }
-    changeText = (event,id)=>{
+    changeText = (event, id) => {
         // alert('Event changed');
         console.log('hey there')
-        let index = this.state.persons.findIndex(p=>{
-            return p.id===id;
+        let index = this.state.persons.findIndex(p => {
+            return p.id === id;
         });
         let prod = {...this.state.persons[index]};
         prod.Name = event.target.value;
@@ -78,23 +85,58 @@ class App extends Component {
             persons: this.state.persons
         })
     }
+
+    changeButton = (e) => {
+
+        console.log(e.target.value)
+        this.setState(
+            this.style_ = {
+                width: e.target.value + 'px',
+                height: e.target.value + 'px'
+            }
+        )
+
+    }
+
+    getContect = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response =>{
+                this.setState(this.state.post = response.data)
+            })
+    }
+
+    sendContent = ()=>{
+        let data = {
+            name : 'sreeraj',
+            colleg : 'GEC thrissur'
+
+        }
+        axios.post('https://portfolio-99b6a.firebaseio.com/data.json' , data)
+            .then(error=>console.log(error))
+    }
+
+
+
     render() {
+
+        // {this.sendContent()}
 
         const style = {
             color: 'black',
-            width:'150px',
-            height:'30px',
-            border : '2px solid blue',
-            borderRadius : '20px',
-            ':hover' : {
-                border : '2px solid green',
-                boxShadow : '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+            width: '150px',
+            height: '30px',
+            border: '2px solid blue',
+            borderRadius: '20px',
+            ':hover': {
+                border: '2px solid green',
+                boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
 
             }
         }
 
         let person = null;
-        if(this.state.show){
+        if (this.state.show) {
+
             person = (<div className="person">
                 {/*<Person*/}
                 {/*    name={this.state.persons[0].Name}*/}
@@ -102,15 +144,20 @@ class App extends Component {
                 {/*    change={this.changeText}*/}
                 {/*    // click = {this.change}*/}
                 {/*/>*/}
-                {this.state.persons.map((person,index)=>
+                {this.state.persons.map((person, index) =>
                     <Person
                         name={person.Name}
                         age={person.Age}
-                        delete={()=>this.DeleteHandler(index)}
+                        delete={() => this.DeleteHandler(index)}
                         key={person.id}
-                        change={(event)=>{this.changeText(event,person.id)}}
+                        change={(event) => {
+                            this.changeText(event, person.id)
+                        }}
                     />
                 )}
+
+                {this.state.post.map((post , index)=>
+                    <h1>{post.title}</h1>)}
             </div>)
         }
 
@@ -118,12 +165,17 @@ class App extends Component {
             <div className="App">
                 {/*{this.state.persons.map((person) =>*/}
                 <h1>Welcome to React</h1>
+                {this.getContect()}
+                {/*<button style={this.style_}>kdfjsldkf</button>*/}
+                <Change
+                        click={this.sendContent}
+                />
                 <button onClick={this.change} style={style}>Show more!!</button>
                 <SlideDown className={'person'}>
                     {person}
                 </SlideDown>
             </div>
-       )
+        )
     }
 }
 
---
+++ src/Person/change.js
@@ -0,0 +1,15 @@
+
+
+import  React , {component} from 'react'
+
+const changeValue = (props)=>{
+
+    return (
+        <div>
+            <button onClick={props.click}>Click me!!</button>
+        </div>
+    )
+
+};
+
+export default changeValue
